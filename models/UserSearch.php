441753196796df8bff1083249eefed00f79058a9
Delpfine <delpfine@mail.ru>
2017-08-28 21:29:46 +0300

    Update UserSearch.php

--- models/UserSearch.php
+++ models/UserSearch.php
@@ -93,7 +93,8 @@ class UserSearch extends Model
             return $dataProvider;
         }
 
-        $table_name = $query->modelClass::tableName();
+        $table_name_class = $query->modelClass;
+        $table_name = $table_name_class::tableName();
 
         if ($this->created_at !== null) {
             $date = strtotime($this->created_at);
